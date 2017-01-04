d9a2b8dd719472a7cac141234608b4d3d9f3a116
Artem Bondar <bondaraw@gmail.com>
2017-01-04 19:16:48 +0200

    Implement logic for custom ViewHolder and Adapter

--- app/src/main/java/com/github/artembondar/myrecyclerview/adapter/MovieAdapter.java
+++ app/src/main/java/com/github/artembondar/myrecyclerview/adapter/MovieAdapter.java
@@ -1,31 +1,57 @@
 package com.github.artembondar.myrecyclerview.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.artembondar.myrecyclerview.R;
+import com.github.artembondar.myrecyclerview.model.Movie;
+
+import java.util.List;
 
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
+    private List<Movie> movies;
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder{
+        public TextView title;
+        public TextView genre;
+        public TextView year;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            genre = (TextView) itemView.findViewById(R.id.genre);
+            year = (TextView) itemView.findViewById(R.id.year);
+        }
+    }
+
+    public MovieAdapter(List<Movie> movies) {
+        this.movies = movies;
+    }
+
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_list_row, parent, false);
+        return new MovieViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position) {
-
+        Movie movie = movies.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
     }
 
-    public class MovieViewHolder extends RecyclerView.ViewHolder{
 
-        public MovieViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
+
 }
